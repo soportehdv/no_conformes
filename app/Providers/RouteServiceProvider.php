3ac71b6a88acb938433211cacc51bb7030b4215c
unknown <Administrador@HDV.LOCAL>
2023-01-26 17:02:50 -0600

    quitando target

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/targets/target';
+    public const HOME = '/';
 
     /**
      * The controller namespace for the application.
